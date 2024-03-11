27f76a4bc3565b9bbbc20e750684bade6662f04d
Alan Vale <valealan84@gmail.com>
2024-03-11 08:02:39 -0300

    Add files via upload

---
+++ assets/js/screamer.js
@@ -0,0 +1,56 @@
+const scream = new Audio('./assets/audio/scream.mp3')
+const canvas = document.querySelector('canvas')
+const img = document.querySelector('img')
+const ctx = canvas.getContext('2d')
+canvas.height = canvas.width
+
+// ---
+
+let pad_position = 0
+const ball = {
+  x: 0.5,
+  y: 0.5,
+  a: 0.5
+}
+
+let bounces = 0
+let speed = 10
+let started
+
+// ---
+
+function render() {
+  move_ball()
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  ctx.fillRect(canvas.width * pad_position, canvas.height - 10, 50, 5)
+  ctx.fillRect(canvas.width * ball.x, canvas.height * ball.y, 10, 10)
+
+  setTimeout(render, speed)
+}
+
+function move_ball() {
+  if (bounces == 5) speed++
+
+  ball.x += Math.cos(ball.a) * 0.01
+  ball.y -= Math.sin(ball.a) * 0.01
+
+  if (ball.x <= 0  || ball.x > (canvas.width  - 10) / canvas.width) 
+    ball.a = Math.PI - ball.a
+
+  if (ball.y <= 0 || ball.y >= (canvas.height - 20) / canvas.height && ball.y < 1 && ball.x >= pad_position && ball.x <= pad_position + (50 / canvas.width)) {
+    ball.a *= -1
+    if (++bounces == 6) {
+      canvas.style.display = 'none'
+      img.style.display = 'block'
+      scream.play()
+    }
+  }
+}
+
+document.body.addEventListener('keydown', e => {
+  if      (e.key == 'ArrowLeft')  pad_position -= 0.03
+  else if (e.key == 'ArrowRight') pad_position += 0.03
+  pad_position = Math.max(0, Math.min((canvas.width - 50) / canvas.width, pad_position))
+})
+
+document.body.addEventListener('click', _ => { if (!started) { started = 1; render() }})
